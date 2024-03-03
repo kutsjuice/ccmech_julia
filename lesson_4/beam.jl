3553582cbb143336ae8db5d9e389b86fbc6de2c2
Kuts Michael <kutsjuice@gmail.com>
2024-03-04 01:11:12 +0300

    eigenmodes computation bugs fixed

--- lesson_4/beam.jl
+++ lesson_4/beam.jl
@@ -10,9 +10,10 @@ const E = 70.0e9
 const ν = 0.33
 const λ = (E*ν)/((1+ν)*(1-2*ν))
 const μ = E/(2*(1+ν))
+const ρ = 7.9e3
 σ(ε) = λ*tr(ε)*one(ε) + 2*μ*ε
 
-domain = (0,10,0,1,0,1)
+domain = (0,0.10,0,0.01,0,0.01)
 partition = (100,10,10)
 model = CartesianDiscreteModel(domain,partition)
 
@@ -40,25 +41,30 @@ U = TrialFESpace(V, [g0])
 # weak form 
 
 # f = VectorValue(0.0, - gravity * ρ)
-A(u,v) = ∫( ε(v) ⊙ (σ∘ε(u)) )*dΩ
+bfK(u,v) = ∫( ε(v) ⊙ (σ∘ε(u)) )*dΩ
 L(v) = 0; #∫(dot(f, v))*dΩ
 
-op_K = AffineFEOperator(A, L, U, V)
+
+# K*x = f
+
+# Mẍ + Kx = 0 
+
+op_K = AffineFEOperator(bfK, L, U, V)
 mK = get_matrix(op_K)
 
-m(u,v) = ρ*∫(v⋅u)dΩ
-op_M = AffineFEOperator(A, L, U, V)
+bfM(u,v) = ρ*∫(v⋅u)dΩ
+op_M = AffineFEOperator(bfM, L, U, V)
 
 mM = get_matrix(op_M)
 
 using Arpack
 
-res = eigs(mK, mM; nev = 10)
+res = eigs(mM, mK; nev = 10, which=:LM)
 res[2]
 vec = real.(res[2])
 
 
-uh_lin = FEFunction(U,vec[:,5])
+uh_lin = FEFunction(U,vec[:,3])
 
 
 res_file = "eigen_freq" |> tcf;
