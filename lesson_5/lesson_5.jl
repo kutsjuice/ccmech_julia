9a8d341d6489551f9e2fa51158b16f13dcb92b47
Kuts Michael <kutsjuice@gmail.com>
2024-09-21 15:05:38 +0300

    fixes bug with redundant edges from rectangle

--- lesson_5/lesson_5.jl
+++ lesson_5/lesson_5.jl
@@ -1,30 +1,31 @@
+# # Построение геометрии
 
-using Gmsh
 
-gmsh.initialize()
+# некоторые вспомагательные функции, которые понадобятся в процессе
+
+findin(a, b) = a[findall(in(b), a)]
 tcf(filename::String) = (@__DIR__) * "\\" * filename;
 
 
-lc = 5e-2
+# Подключаем gmsh и инциализируем его, после чего моздаем модель и задаем базовый размер репрезентативной ячейки:
+using Gmsh
+
+gmsh.initialize()
 
 factory = gmsh.model.occ
 gmsh.model.add("model")
 
+cell_size = 1;
 
-# p1 = factory.addPoint(0,0,0, lc);
-# p2 = factory.addPoint(1,0,0, lc);
-# p3 = factory.addPoint(1,1,0, lc);
-# p4 = factory.addPoint(0,1,0, lc);
+# Исследуемая геометрия чем-то напомнимает сыр и представляет собой прямоугольник с крыглыми отверстиями. Для этого создадим прямоугольник размером `cell_size`, 
+# затем некоторое количество окружностей, которые в дальнейшем будут вырезаны из прямоугольной области. Следует заметить, что булевы операции возможны лишь для 2D или 3D объектов,
+# однако прямоугольник и окружность это по своей сути 1D объект. Поэтому прежде чем произвести операцию вырезки, необходимо "натянуть" на созданные контура прямоугольника и окружности 
+# поверхности при помощи функции `addPlaneSurface`.
 
-# l1 = factory.addLine(p1, p2);
-# l2 = factory.addLine(p2, p3);
-# l3 = factory.addLine(p3, p4);
-# l4 = factory.addLine(p4, p1);
+rect = factory.addRectangle(-0.5 * cell_size, -0.5 * cell_size, 0, cell_size, cell_size);
 
-
-rect = factory.addRectangle(-0.5, -0.5, 0, 1, 1);
-# outter_loop = factory.addCurveLoop([c1]);
-full_domain = factory.addPlaneSurface([rect])
+factory.getEntities(2)
+# full_domain = factory.addPlaneSurface([rect])
 
 holes = [];
 for i in 1:20
@@ -37,54 +38,134 @@ for i in 1:20
 end
 
 
-diff = factory.cut([(2,full_domain)], holes, -1, false, true)
-
-# println(diff)
-# frag = factory.fragment(diff[0], [(2, Surface2)])    
-# print(frag1)
-
-# filled_entities = diff[2][end]
-# main_entities = copy(diff[1])
-# for entity in filled_entities
-#   if entity in main_entities
-    # main_entities.remove(entity)
-#   end
-# end
-# print("  ", main_entities, "\n")
-factory.synchronize()
-
-entities = gmsh.model.occ.getEntities(2)
-gmsh.model.occ.remove(entities[1:2])
+diff = factory.cut([rect], holes, -1)
 
 factory.synchronize()
 
-for dim in 2:2
-    entities = gmsh.model.occ.getEntities(dim)
-    tags = [entity[2] for entity in entities]
-    gmsh.model.addPhysicalGroup(dim, tags, -1, "domain")
+#№ После соз
+
+#№ entities = gmsh.model.occ.getEntities(2)
+#№ gmsh.model.occ.remove(entities[1:1])
+
+
+#№ factory.synchronize()
+
+
+entities = factory.getEntities(2)
+
+surface_boundary = gmsh.model.getBoundary(entities)
+
+
+
+edges_for_leave = [abs(tag[2]) for tag in surface_boundary]
+
+all_edges = [abs(tag[2]) for tag in factory.getEntities(1)]
+
+edges_for_remove = []
+for edge in all_edges
+    if  !(edge in edges_for_leave)
+        push!(edges_for_remove, edge)
+    end
 end
+
+dimtag_for_remove = [(1, tag) for tag in edges_for_remove]
+factory.remove(dimtag_for_remove)
+
+factory.synchronize()
+
+dim = 2
+entities = gmsh.model.occ.getEntities(dim)
+tags = [entity[2] for entity in entities]
+gmsh.model.addPhysicalGroup(dim, tags, -1, "domain")
+
+# Создаем физические группы для границ
+
+eps = cell_size * 1e-4;
+left_boundary_bbox = [
+    (- cell_size/2 - eps),
+    (- cell_size/2 - eps),
+    (- eps),
+    (- cell_size/2 + eps),
+    (+ cell_size/2 + eps),
+    (+ eps)
+]
+right_boundary_bbox = [
+    (+ cell_size/2 - eps),
+    (- cell_size/2 - eps),
+    (- eps),
+    (+ cell_size/2 + eps),
+    (+ cell_size/2 + eps),
+    (+ eps)
+]
+bottom_boundary_bbox = [
+    (- cell_size/2 - eps),
+    (- cell_size/2 - eps),
+    (- eps),
+    (+ cell_size/2 + eps),
+    (- cell_size/2 + eps),
+    (+ eps)
+]
+top_boundary_bbox = [
+    (- cell_size/2 - eps),
+    (+ cell_size/2 - eps),
+    (- eps),
+    (+ cell_size/2 + eps),
+    (+ cell_size/2 + eps),
+    (+ eps)
+]
+
+edge_bboxes = Dict(
+    "left" => left_boundary_bbox, 
+    "right" => right_boundary_bbox,
+    "bottom" => bottom_boundary_bbox,
+    "top" => top_boundary_bbox
+)
+
+node_bboxes = Dict(
+    "top_right" => (top_boundary_bbox, right_boundary_bbox),
+    "top_left" => (top_boundary_bbox, left_boundary_bbox), 
+    "bottom_right" => (bottom_boundary_bbox, right_boundary_bbox), 
+    "bottom_left" => (bottom_boundary_bbox, left_boundary_bbox)
+)
+
+for (boundary_name, bbox) in edge_bboxes
+    dim = 1
+    entities = gmsh.model.occ.getEntitiesInBoundingBox(bbox..., dim)   
+    tags = [entity[2] for entity in entities]
+    gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
+end
+
+for (boundary_name, bbox) in node_bboxes
+    dim = 0
+    entities1 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[1]..., dim)   
+    entities2 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[2]..., dim)   
+    entities = findin(entities1, entities2)
+    tags = [entity[2] for entity in entities]
+    gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
+end
+
 
 gmsh.model.occ.synchronize()
 
 gmsh.option.setNumber("Mesh.MeshSizeMin", 0.01)
 gmsh.option.setNumber("Mesh.MeshSizeMax", 0.05)
-# gmsh.option.setNumber("Mesh.RecombineAll", 1)
+## gmsh.option.setNumber("Mesh.RecombineAll", 1)
 gmsh.option.setNumber("Mesh.Algorithm", 8)
-# gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 3) # or 3
-# gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
-gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 30)
+## gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 3) # or 3
+## gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 10)
 
 gmsh.model.mesh.generate(2)
-# gmsh.model.mesh.recombine()
-# gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
-# gmsh.model.mesh.refine()
-# gmsh.model.mesh.refine()
+## gmsh.model.mesh.recombine()
+## gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+## gmsh.model.mesh.refine()
+## gmsh.model.mesh.refine()
 
 
-# RecombineMesh
-if !("-nopopup" in ARGS)
-    gmsh.fltk.run()
-end
+
+## if !("-nopopup" in ARGS)
+##     gmsh.fltk.run()
+## end
 
 msh_file = "porous_media.msh" |> tcf;
 print(msh_file)
@@ -92,11 +173,11 @@ gmsh.write(msh_file)
 
 gmsh.finalize()
 
-##
+#
 using Gridap
 using GridapGmsh
 
 model = GmshDiscreteModel(msh_file);
 
-vtk_file = "model" |> tcf;
+vtk_file = "porous_model" |> tcf;
 writevtk(model, vtk_file);
