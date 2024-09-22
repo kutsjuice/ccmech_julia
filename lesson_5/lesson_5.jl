59ee219580260aa0b8a472ca9defeaf288fd4fd4
Kuts Michael <kutsjuice@gmail.com>
2024-09-22 22:40:02 +0300

    adds solution for 3 boundary value problems

--- lesson_5/lesson_5.jl
+++ lesson_5/lesson_5.jl
@@ -17,17 +17,20 @@ gmsh.model.add("model")
 
 cell_size = 1;
 
-# Исследуемая геометрия чем-то напомнимает сыр и представляет собой прямоугольник с крыглыми отверстиями. Для этого создадим прямоугольник размером `cell_size`, 
-# затем некоторое количество окружностей, которые в дальнейшем будут вырезаны из прямоугольной области. Следует заметить, что булевы операции возможны лишь для 2D или 3D объектов,
-# однако прямоугольник и окружность это по своей сути 1D объект. Поэтому прежде чем произвести операцию вырезки, необходимо "натянуть" на созданные контура прямоугольника и окружности 
-# поверхности при помощи функции `addPlaneSurface`.
+# Исследуемая геометрия чем-то напомнимает сыр и представляет собой прямоугольник с крыглыми отверстиями. 
+# Для этого создадим прямоугольник размером `cell_size`, затем некоторое количество окружностей, которые в дальнейшем будут вырезаны из прямоугольной области. 
+# Следует заметить, что булевы операции возможны лишь для 2D или 3D объектов. 
+# Тут необходимо учесть некоторую особонность построения геометрии в **gmsh** - при создании прямоугольника, будет созданы необходимые отрезки, замкнутый контур и участок поверхности, ограниченный данным контуром. 
+# В случае же окружностей будут созданы только лишь замкнутые дуги, т.е. 1D объекты. 
+# Поэтому прежде чем произвести операцию вырезки, необходимо создать контур для каждой окружности (состоящий из одной окружности) и "натянуть" на созданные контура поверхности при помощи функции `addPlaneSurface`.
 
 rect = factory.addRectangle(-0.5 * cell_size, -0.5 * cell_size, 0, cell_size, cell_size);
 
-factory.getEntities(2)
-# full_domain = factory.addPlaneSurface([rect])
+#src # factory.getEntities(2)
+#src # full_domain = factory.addPlaneSurface([rect])
 
 holes = [];
+
 for i in 1:20
     xy = rand(2) .- 0.5;;
     r = 0.05;
@@ -37,25 +40,18 @@ for i in 1:20
     push!(holes, (2, hole));
 end
 
+# Теперь можно удалить созданные кругляши из прямоугольника, воспользовавшись функцией `cut`
 
-diff = factory.cut([rect], holes, -1)
+diff = factory.cut([(2, rect)], holes, -1, true, true)
 
 factory.synchronize()
 
-#№ После соз
-
-#№ entities = gmsh.model.occ.getEntities(2)
-#№ gmsh.model.occ.remove(entities[1:1])
-
-
-#№ factory.synchronize()
-
+# В некоторых случаях может оставаться избыточная геометрия, которая будет мешать создать модель. 
+# Поэтому удалим все 1D объекты, которые не входят в границу нашей ячейки
 
 entities = factory.getEntities(2)
 
 surface_boundary = gmsh.model.getBoundary(entities)
-
-
 
 edges_for_leave = [abs(tag[2]) for tag in surface_boundary]
 
@@ -73,12 +69,16 @@ factory.remove(dimtag_for_remove)
 
 factory.synchronize()
 
-dim = 2
-entities = gmsh.model.occ.getEntities(dim)
-tags = [entity[2] for entity in entities]
-gmsh.model.addPhysicalGroup(dim, tags, -1, "domain")
+# Далее необходимо создаnm физические группы для созданной геометрией. Группа для всей геометрии:
+for dim in 0:2
+    entities = gmsh.model.occ.getEntities(dim)
+    tags = [entity[2] for entity in entities]
+    gmsh.model.addPhysicalGroup(dim, tags, -1, "domain")
+end
 
-# Создаем физические группы для границ
+# Далее создаем физические группы для границ.
+# Границы можно выделить при помощи инструмента `boundingBox`, что напоминает выделение мышкой на экране прямоугольной области. 
+# Для начала создадим 4 области, в которые входят боковые границы ячейки: 
 
 eps = cell_size * 1e-4;
 left_boundary_bbox = [
@@ -114,70 +114,146 @@ top_boundary_bbox = [
     (+ eps)
 ]
 
-edge_bboxes = Dict(
-    "left" => left_boundary_bbox, 
-    "right" => right_boundary_bbox,
-    "bottom" => bottom_boundary_bbox,
-    "top" => top_boundary_bbox
-)
+bounds = [left_boundary_bbox, bottom_boundary_bbox, right_boundary_bbox, top_boundary_bbox]
 
-node_bboxes = Dict(
-    "top_right" => (top_boundary_bbox, right_boundary_bbox),
-    "top_left" => (top_boundary_bbox, left_boundary_bbox), 
-    "bottom_right" => (bottom_boundary_bbox, right_boundary_bbox), 
-    "bottom_left" => (bottom_boundary_bbox, left_boundary_bbox)
-)
 
-for (boundary_name, bbox) in edge_bboxes
-    dim = 1
-    entities = gmsh.model.occ.getEntitiesInBoundingBox(bbox..., dim)   
+#src # # Далее сгруппируем эти области в 2 словаря: первый используется, для выделения боковых граней
+#src # edge_bboxes = Dict(
+#src #     "left" => left_boundary_bbox, 
+#src #     "right" => right_boundary_bbox,
+#src #     "bottom" => bottom_boundary_bbox,
+#src #     "top" => top_boundary_bbox
+#src # )
+
+#src # node_bboxes = Dict(
+#src #     "top_right" => (top_boundary_bbox, right_boundary_bbox),
+#src #     "top_left" => (top_boundary_bbox, left_boundary_bbox), 
+#src #     "bottom_right" => (bottom_boundary_bbox, right_boundary_bbox), 
+#src #     "bottom_left" => (bottom_boundary_bbox, left_boundary_bbox)
+#src # )
+
+#src # for (boundary_name, bbox) in edge_bboxes
+#src #     dim = 1
+#src #     entities = gmsh.model.occ.getEntitiesInBoundingBox(bbox..., dim)   
+#src #     tags = [entity[2] for entity in entities]
+#src #     gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
+#src # end
+
+#src # for (boundary_name, bbox) in node_bboxes
+#src #     dim = 0
+#src #     entities1 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[1]..., dim)   
+#src #     entities2 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[2]..., dim)   
+#src #     entities = findin(entities1, entities2)
+#src #     tags = [entity[2] for entity in entities]
+#src #     gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
+#src # end
+
+# Далее создадим две физические группы: с узлами (1D объекты) и с ребрами (2D объекты)
+
+boundary_name = "boundary";
+for dim in 0:1
+    entities = []
+    for bbox in bounds
+        push!(entities, factory.getEntitiesInBoundingBox(bbox..., dim)...)
+    end
     tags = [entity[2] for entity in entities]
     gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
 end
-
-for (boundary_name, bbox) in node_bboxes
-    dim = 0
-    entities1 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[1]..., dim)   
-    entities2 = gmsh.model.occ.getEntitiesInBoundingBox(bbox[2]..., dim)   
-    entities = findin(entities1, entities2)
-    tags = [entity[2] for entity in entities]
-    gmsh.model.addPhysicalGroup(dim, tags, -1, boundary_name)
-end
-
-
 gmsh.model.occ.synchronize()
+
+# Теперь осталось задать параметры сетки и запустить генерацию сетки, после чего созданная геометрия может быть сохранена в файл
+
 
 gmsh.option.setNumber("Mesh.MeshSizeMin", 0.01)
 gmsh.option.setNumber("Mesh.MeshSizeMax", 0.05)
-## gmsh.option.setNumber("Mesh.RecombineAll", 1)
+#src # gmsh.option.setNumber("Mesh.RecombineAll", 1)
 gmsh.option.setNumber("Mesh.Algorithm", 8)
-## gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 3) # or 3
-## gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
-gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 10)
+#src # gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 3) # or 3
+#src # gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+gmsh.option.setNumber("Mesh.MeshSizeFromCurvature", 20)
 
 gmsh.model.mesh.generate(2)
-## gmsh.model.mesh.recombine()
-## gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
-## gmsh.model.mesh.refine()
-## gmsh.model.mesh.refine()
-
-
-
-## if !("-nopopup" in ARGS)
-##     gmsh.fltk.run()
-## end
+#src # gmsh.model.mesh.recombine()
+#src # gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+#src # gmsh.model.mesh.refine()
+#src
+#src # if !("-nopopup" in ARGS)
+#src #      gmsh.fltk.run()
+#src # end
 
 msh_file = "porous_media.msh" |> tcf;
-print(msh_file)
+
 gmsh.write(msh_file)
 
 gmsh.finalize()
+#src
+# Теперь необходимо загрузить нашу геометрию при помощи Gridap и 
 
-#
 using Gridap
 using GridapGmsh
 
 model = GmshDiscreteModel(msh_file);
 
 vtk_file = "porous_model" |> tcf;
-writevtk(model, vtk_file);
+#src # writevtk(model, vtk_file);
+
+
+# Зададим основные механические параметры для материала и определим закон для напряжений (закон Гука):
+const E = 70.0e9
+const ν = 0.33
+const λ = (E*ν)/((1+ν)*(1-2*ν))
+const μ = E/(2*(1+ν))
+const ρ = 2.8e3
+σ(ε) = λ*tr(ε)*one(ε) + 2*μ*ε
+
+# Задаем пространство тестовых функций. В качестве границы Дирихле укажем границу с тегом `boundary` которую мы создали в **gmsh**
+degree = 2
+Ω = Triangulation(model)
+dΩ = Measure(Ω,degree)
+
+reffe = ReferenceFE(lagrangian, VectorValue{2,Float64}, 1)
+V = TestFESpace(model, reffe, 
+    conformity=:H1,
+    dirichlet_tags = [boundary_name]
+)
+
+# Теперь нам необходимо решить 3 краевых задач, каждой из которых соответствуют свои граничные условия.
+
+g1(x) = VectorValue(x[1],  0.0)
+g2(x) = VectorValue(0.0,  x[2])
+g3(x) = VectorValue(x[2]/(2cell_size),  x[1]/(2cell_size))
+    
+U1 = TrialFESpace(V, [g1])
+U2 = TrialFESpace(V, [g2])
+U3 = TrialFESpace(V, [g3])
+
+U = [U1, U2, U3]
+
+# Слабая постановка для всех трех задач будет одинаковая
+
+A(u,v) = ∫( ε(v) ⊙ (σ∘ε(u)) )*dΩ
+L(v) = 0; 
+
+# Теперь решаем все три задачи и сохраняем результат в файл
+res_file = "results_new" |> tcf;
+result = []
+for i in 1:3 
+    op = AffineFEOperator(A, L, U[i], V)
+
+    x0 = zeros(Float64, num_free_dofs(V))
+    uh_lin = FEFunction(U[i], x0)
+    ls = LUSolver()
+    solver = LinearFESolver(ls)
+    uh_lin ,_ = solve!(uh_lin,solver,op)
+    push!(result, uh_lin);
+
+end
+writevtk(Ω, res_file ,  cellfields=[
+    "x_tension_displ" => result[1],
+    "y_tension_displ" => result[2],
+    "xy_shear_displ" => result[3],
+    "x_tension_stress"=> σ∘ε(result[1]),
+    "y_tension_stress"=> σ∘ε(result[2]),
+    "xy_shear_stress"=> σ∘ε(result[3]),
+    ]
+)
